edcaeae7fa34089ac0942b7a901c118376653c3b
dadacm <davimarque2000@gmail.com>
2021-03-24 19:36:42 -0300

    5. verifica se e triangulo pelo valor dos angulos

--- exercices/introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ exercices/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -1,21 +1,21 @@
-let a = 5;
-let b = 10;
-let c = 15;
+// let a = 5;
+// let b = 10;
+// let c = 15;
 // adição
-let soma = a + b;
-console.log (soma);
+// let soma = a + b;
+// console.log (soma);
 // subtração
-let subtracao = a - b ;
-console.log (subtracao);
+// let subtracao = a - b ;
+// console.log (subtracao);
 // divisão
-let divisao = b / a;
-console.log (divisao);
+// let divisao = b / a;
+// console.log (divisao);
 // multiplicação
-let multiplicacao = a * b;
-console.log (multiplicacao);
+// let multiplicacao = a * b;
+// console.log (multiplicacao);
 // modulo
-let modulo = a % b;
-console.log(modulo)
+// let modulo = a % b;
+// console.log(modulo)
 // if(a > b){
     // console.log('o', a, 'é maior que o', b);
 // }
@@ -31,12 +31,26 @@ console.log(modulo)
 // else{
     // console.log (c, 'é o maior numero')
 // }
-if(a > 0) {
-    console.log('Positive');
+// if(a > 0) {
+    // console.log('Positive');
+// }
+// else if (a < 0){
+    // console.log('negative');
+// }
+// else{
+    // console.log ('0')
+// }
+let angulo1= -30;
+let angulo2= 50;
+let angulo3= 90;
+if(angulo1 + angulo2 + angulo3 === 180){
+let triangulo = true;
+console.log (triangulo);
 }
-else if (a < 0){
-    console.log('negative');
+else if(angulo1 < 0 || angulo2 < 0 || angulo3 < 0){
+    console.log('Valor de angulo invalido');
 }
 else{
-    console.log ('0')
+    let triangulo = false;
+    console.log(triangulo)
 }
