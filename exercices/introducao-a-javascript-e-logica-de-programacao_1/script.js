5ac7a96d2f40c8a311da57a72b9bdf88dc9255cb
dadacm <davimarque2000@gmail.com>
2021-03-24 19:12:58 -0300

    3. retorna o maior entre 3 numeros

--- exercices/introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ exercices/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -1,5 +1,6 @@
 let a = 5;
 let b = 10;
+let c = 15;
 // adição
 let soma = a + b;
 console.log (soma);
@@ -15,9 +16,18 @@ console.log (multiplicacao);
 // modulo
 let modulo = a % b;
 console.log(modulo)
-if(a > b){
-    console.log('o', a, 'é maior que o', b);
+// if(a > b){
+    // console.log('o', a, 'é maior que o', b);
+// }
+// else{
+    // console.log('o', b, 'é maior que o', a);
+// }
+if(a>b & a>c){
+    console.log (a, 'é o maior numero');
+}
+else if (b > a & b>c){
+    console.log (b, 'é o maior numero');
 }
 else{
-    console.log('o', b, 'é maior que o', a);
+    console.log (c, 'é o maior numero')
 }
